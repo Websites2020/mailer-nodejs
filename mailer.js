050c70d745377cbf9b5b976e0ee98a102865d988
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-01 15:47:44 -0400

    first

---
+++ mailer.js
@@ -0,0 +1,23 @@
+var nodemailer= require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user:'<email>',
+        pass:'<password>'
+    }
+});
+
+var mailOptions = {
+    from: '<email>',
+    to: '<email>',
+    subject: 'Sending Email using Node.js',
+    html: '<h1>Welcome</h1><p>That was easy!</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
